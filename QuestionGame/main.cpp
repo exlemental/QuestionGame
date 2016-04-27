e62b41c2d5c79029e6cbaded105fb69de93e33f9
Kerem ER <Kerem@172-15-10-131.lightspeed.snjsca.sbcglobal.net>
2016-04-27 20:31:06 +0300

    Initial Commit

---
+++ QuestionGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuestionGame
+//
+//  Created by Kerem ER on 27.04.2016.
+//  Copyright © 2016 KeremEr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
